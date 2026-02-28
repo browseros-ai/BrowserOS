dc651fa1d1c70310f1a7dad07c01ccc02001c0ff
Nikhil Sonti <nikhilsv92@gmail.com>
2026-02-27 18:42:19 -0800

    feat: improve bros pull/push sync and patch safety

---
+++ packages/browseros/tools/bros/cmd/sync_args.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"bros/internal/git"
+)
+
+func resolveRemoteAndFiles(repoDir string, args []string, explicitRemote string) (string, []string, error) {
+	remote := strings.TrimSpace(explicitRemote)
+	if remote != "" {
+		hasRemote, err := git.HasRemote(repoDir, remote)
+		if err != nil {
+			return "", nil, fmt.Errorf("resolving remote %q: %w", remote, err)
+		}
+		if !hasRemote {
+			return "", nil, fmt.Errorf("remote %q not found in patches repo", remote)
+		}
+		return remote, args, nil
+	}
+
+	if len(args) == 0 {
+		return "", nil, nil
+	}
+
+	hasRemote, err := git.HasRemote(repoDir, args[0])
+	if err != nil {
+		return "", nil, fmt.Errorf("resolving remote %q: %w", args[0], err)
+	}
+	if hasRemote {
+		return args[0], args[1:], nil
+	}
+
+	return "", args, nil
+}
